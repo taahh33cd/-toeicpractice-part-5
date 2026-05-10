a8227394b71ca612e6f003f25054575582b8d468
taah <127932835+taahh33cd@users.noreply.github.com>
2026-05-10 08:29:36 -0700

    Delete scratch directory

--- scratch/count_mdqh.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const data = fs.readFileSync('c:/Users/tsdie/.gemini/antigravity/scratch/toeic-part5-app/data/questions.js', 'utf8');
-const matches = data.match(/"grammar_type":\s*"MĐQH"/g);
-console.log(matches ? matches.length : 0);
