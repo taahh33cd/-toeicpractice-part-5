b4991c02a1fe7e5c4739a192480f394a3e7ab97b
taah <127932835+taahh33cd@users.noreply.github.com>
2026-05-12 05:29:51 -0700

    Delete scratch directory

--- scratch/fix_syntax.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let data = fs.readFileSync('data/questions.js', 'utf8');
-data = data.replace(/,\\n/g, ',\n');
-data = data.replace(/\\n\];/g, '\n];');
-fs.writeFileSync('data/questions.js', data);
-console.log('Fixed file');
